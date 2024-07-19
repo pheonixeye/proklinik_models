9c5ed8d49f8a5dbd163e5d11b0faa46ac77ba75c
pheonixeye <drkazgm@gmail.com>
2024-07-19 18:12:39 +0300

    added notification type

--- lib/models/server_notification.dart
+++ lib/models/server_notification.dart
@@ -2,12 +2,14 @@ class ServerNotification {
   ServerNotification({
     required this.token,
     required this.title,
+    required this.notificationType,
     this.body,
     this.data,
   });
 
   final String? token;
   final String title;
+  final NotificationType notificationType;
   final String? body;
   final Map<String, dynamic>? data;
 
@@ -19,8 +21,28 @@ class ServerNotification {
           'title': title,
           'body': body ?? '',
         },
-        'data': data ?? {},
+        'data': data == null
+            ? {}
+            : {'notification_type': notificationType.value, ...data!},
       },
+    };
+  }
+}
+
+enum NotificationType {
+  newBooking('new_booking'),
+  newInvoice('new_invoice'),
+  unknown('unknown');
+
+  final String value;
+
+  const NotificationType(this.value);
+
+  factory NotificationType.fromString(String value) {
+    return switch (value) {
+      'new_booking' => NotificationType.newBooking,
+      'new_invoice' => NotificationType.newInvoice,
+      _ => NotificationType.unknown,
     };
   }
 }
